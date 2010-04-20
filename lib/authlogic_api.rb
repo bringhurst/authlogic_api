1ec8e2f8c1bee7eab23d2c25aa59ce9fac2f5df0
bpalmen <bpalmen@cyber.law.harvard.edu>
2010-04-20 16:30:58 -0400

    Adding initial nonce validation features

--- lib/authlogic_api.rb
+++ lib/authlogic_api.rb
@@ -1,5 +1,6 @@
 require "authlogic_api/acts_as_authentic"
 require "authlogic_api/session"
+require "authlogic_api/nonce"
 
 ActiveRecord::Base.send(:include, AuthlogicApi::ActsAsAuthentic)
 Authlogic::Session::Base.send(:include, AuthlogicApi::Session)
